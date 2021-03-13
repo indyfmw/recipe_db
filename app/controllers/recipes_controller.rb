b378f36d0eb992e3b343436d9f4cb33b53610029
indyfmw <indyfmw@gmail.com>
2021-03-13 04:48:35 -0600

    assignment 4

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -64,7 +64,9 @@ class RecipesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def recipe_params
-      params.require(:recipe).permit(:title, :description, ingredients_attributes:[:id, :content, :_destroy], steps_attributes:[:id, :direction, :_destroy])
+      params.require(:recipe).permit(:remove_image, :image, :title, :description, ingredients_attributes:[:id, :content, :_destroy], steps_attributes:[:id, :direction, :_destroy])
+
+
 
 
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_12_201932) do
+ActiveRecord::Schema.define(version: 2021_03_13_084751) do
 
   create_table "ingredients", force: :cascade do |t|
     t.text "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_03_12_201932) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "steps", force: :cascade do |t|
